8bdad84585c1101363952815c4456aa621175a6d
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2016-05-06 09:16:06 +0100

    Removed unused user field and updated error capture to include json request

--- RemoteSpiNNaker/RemoteSpiNNakerRESTUtils/src/main/java/uk/ac/manchester/cs/spinnaker/rest/ErrorCaptureResponseFilter.java
+++ RemoteSpiNNaker/RemoteSpiNNakerRESTUtils/src/main/java/uk/ac/manchester/cs/spinnaker/rest/ErrorCaptureResponseFilter.java
@@ -1,6 +1,7 @@
 package uk.ac.manchester.cs.spinnaker.rest;
 
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.List;
 
 import javax.ws.rs.client.ClientRequestContext;
@@ -10,8 +11,13 @@ import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response.Status.Family;
 import javax.ws.rs.ext.Provider;
 
+import org.apache.commons.io.output.WriterOutputStream;
+
 @Provider
 public class ErrorCaptureResponseFilter implements ClientResponseFilter {
+
+    private CustomJacksonJsonProvider provider =
+        new CustomJacksonJsonProvider();
 
     @Override
     public void filter(ClientRequestContext requestContext,
@@ -32,8 +38,29 @@ public class ErrorCaptureResponseFilter implements ClientResponseFilter {
                         + headerValue);
                 }
             }
+            String json = null;
+            try {
+                StringWriter jsonWriter = new StringWriter();
+                WriterOutputStream jsonOutput = new WriterOutputStream(
+                    jsonWriter);
+                provider.writeTo(
+                    requestContext.getEntity(), requestContext.getEntityClass(),
+                    requestContext.getEntityType(),
+                    requestContext.getEntityAnnotations(),
+                    requestContext.getMediaType(), requestContext.getHeaders(),
+                    jsonOutput);
+                jsonOutput.close();
+                jsonWriter.close();
+                json = jsonWriter.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             System.err.println("    Entity:");
             System.err.println("        " + requestContext.getEntity());
+            if (json != null) {
+                System.err.println("    JSON version:");
+                System.err.println("        " + json);
+            }
         }
     }
 

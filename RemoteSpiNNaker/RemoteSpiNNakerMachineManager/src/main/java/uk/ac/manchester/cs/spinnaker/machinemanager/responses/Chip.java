c4fd2763199b2bb4e5c1d651d2e913428c21d2b0
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2016-11-09 14:49:14 +0000

    Added testing of deserialization of responses.

--- RemoteSpiNNaker/RemoteSpiNNakerMachineManager/src/main/java/uk/ac/manchester/cs/spinnaker/machinemanager/responses/Chip.java
+++ RemoteSpiNNaker/RemoteSpiNNakerMachineManager/src/main/java/uk/ac/manchester/cs/spinnaker/machinemanager/responses/Chip.java
@@ -1,27 +1,28 @@
 package uk.ac.manchester.cs.spinnaker.machinemanager.responses;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import static com.fasterxml.jackson.annotation.JsonFormat.Shape.ARRAY;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({"x", "y"})
-@JsonFormat(shape=JsonFormat.Shape.ARRAY)
+@JsonPropertyOrder({ "x", "y" })
+@JsonFormat(shape = ARRAY)
 public class Chip {
-    private int x;
-    private int y;
+	private int x;
+	private int y;
 
-    public int getX() {
-        return x;
-    }
+	public int getX() {
+		return x;
+	}
 
-    public void setX(int x) {
-        this.x = x;
-    }
+	public void setX(int x) {
+		this.x = x;
+	}
 
-    public int getY() {
-        return y;
-    }
+	public int getY() {
+		return y;
+	}
 
-    public void setY(int y) {
-        this.y = y;
-    }
+	public void setY(int y) {
+		this.y = y;
+	}
 }
--- RemoteSpiNNaker/RemoteSpiNNakerMachineManager/src/main/java/uk/ac/manchester/cs/spinnaker/machinemanager/responses/ReturnResponse.java
+++ RemoteSpiNNaker/RemoteSpiNNakerMachineManager/src/main/java/uk/ac/manchester/cs/spinnaker/machinemanager/responses/ReturnResponse.java
@@ -4,15 +4,14 @@ import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.databind.JsonNode;
 
 public class ReturnResponse implements Response {
+	private String returnValue;
 
-    private String returnValue;
+	public String getReturnValue() {
+		return returnValue;
+	}
 
-    public String getReturnValue() {
-        return returnValue;
-    }
-
-    @JsonSetter("return")
-    public void setReturnValue(JsonNode returnValue) {
-        this.returnValue = returnValue.toString();
-    }
+	@JsonSetter("return")
+	public void setReturnValue(JsonNode returnValue) {
+		this.returnValue = returnValue.toString();
+	}
 }
